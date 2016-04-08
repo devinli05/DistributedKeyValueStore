44dbc3e580b3faaaa7a6ae0d92c949e30f859192
Yongnan Li <v0r5@ugrad.cs.ubc.ca>
2016-04-08 11:13:46 -0700

    modify script to use bash instead of ./ so don't need to run chmod

--- setupLinuxNodes.sh
+++ setupLinuxNodes.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-./setuplin10.sh &
+bash setuplin10.sh &
 sleep 5
-./setuplin11.sh &
-./setuplin12.sh &
-./setuplin13.sh &
-./setuplin14.sh &
-./setuplin15.sh &
-./setuplin16.sh &
-./setuplin17.sh &
+bash setuplin11.sh &
+bash setuplin12.sh &
+bash setuplin13.sh &
+bash setuplin14.sh &
+bash setuplin15.sh &
+bash setuplin16.sh &
+bash setuplin17.sh &
