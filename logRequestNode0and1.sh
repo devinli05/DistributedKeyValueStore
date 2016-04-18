3fc2524255a89feeb7d38007291375d165e8390a
Yongnan Li <v0r5@ugrad.cs.ubc.ca>
2016-04-17 17:02:08 -0700

    gather govec logs in file shiviz.log

---
+++ logRequestNode0and1.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+cat regex.txt > shiviz.log
+cat HttpNodeLog0-Log.txt >> shiviz.log
+cat HttpNodeLog1-Log.txt >> shiviz.log
+cat 0-Log.txt >> shiviz.log
+cat 1-Log.txt >> shiviz.log
+cat 2-Log.txt >> shiviz.log
+cat 3-Log.txt >> shiviz.log
+cat 4-Log.txt >> shiviz.log
+cat 5-Log.txt >> shiviz.log
+cat 6-Log.txt >> shiviz.log
+cat 7-Log.txt >> shiviz.log
